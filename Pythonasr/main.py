2ff7c26ac793fa85f73b115cb7c14011381a0ca9
NizuSAO <nizusao@gmail.com>
2022-04-11 00:59:00 -0300

    link start

--- Pythonasr/main.py
+++ Pythonasr/main.py
@@ -1,10 +1,12 @@
 import requests
 
 if __name__ == '__main__':
-    x = requests.get('https://api.nasa.gov/planetary/apod?api_key=bOxO1Yeu9ffCWJ6iayhXvx8E64i3otvkdKG1rwxA')
+    cabeceras = {'cache-control': 'no-cache', 'accept': 'text/html'}
+    x = requests.get('https://api.nasa.gov/planetary/apod?api_key=wg7aYLGtx2cMt16hRSjoUfze2UvOuc7s0ueXIZ65', headers = cabeceras)
+    x.headers
 
     if x.status_code == 200:
         content = x.content
-        file = open('nasa.html', 'wb')
+        file = open('nasaheader.txt', 'wb')
         file.write(content)
         file.close()
