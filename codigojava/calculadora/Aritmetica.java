b8945e275733bdc307e397caa4a2aed89038fde7
NizuSAO <93949428+NizuSAO@users.noreply.github.com>
2022-10-06 22:20:49 -0300

    Update Aritmetica.java

--- codigojava/calculadora/Aritmetica.java
+++ codigojava/calculadora/Aritmetica.java
@@ -1,8 +1,10 @@
 package calculadora;
 
 public class Aritmetica {
-	int numero1 = 10;
+	int numero1 = 2;
+	int numero1negativo = -2;
 	int numero2 = 5;
+	int numero3 = 3;
 	int suma;
 	int resta;
 	int multiplicacion;
@@ -31,4 +33,15 @@ public class Aritmetica {
 		division= numero1/numero2;
 		System.out.print("La division es:" + division);
 	}
+	public void cuadratica() {
+		 double potencia = numero1-4*numero2*numero3;
+		 
+         double x1 = (numero1negativo - Math.sqrt(potencia)/ 2*numero2);
+
+         double x2 = (numero1negativo + Math.sqrt(potencia)/ 2*numero2);
+		
+		System.out.printf("la x1 es: \n" + x1);
+		System.out.printf("la x2 es: \n" + x2);
+	}
+}
 }
