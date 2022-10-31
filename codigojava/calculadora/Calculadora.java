6294a81f575cc3d7f9d326e4464f0d22ff6074e9
NizuSAO <93949428+NizuSAO@users.noreply.github.com>
2022-10-31 19:15:53 -0300

    Update Calculadora.java

--- codigojava/calculadora/Calculadora.java
+++ codigojava/calculadora/Calculadora.java
@@ -13,7 +13,20 @@ public class Calculadora {
 	public int division (int a, int b) {
 		return a/b;
 	}
-	
+	public int Cuadratica () {
+		float[]Ema(float a, float b, float c){
+			float cuadrado = b*b;
+			float raiz = math.sqrt(cuadrado=4.a.c);
+			float cuenta = (float) raiz;
+			float verifico = cuenta;
+			if(verifico/2=0){
+				float VP = (((-b)+cuenta)/(2*a));
+				float VM = (((-b)-cuenta)/(2*a));
+				float[]Resultado {VP,VM};
+				return Resultado;
+			}
+		}
+			
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
