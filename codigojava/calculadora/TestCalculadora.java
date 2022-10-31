27e2a9b80e5b1ff134dbf5c5b5b7cb10c42163c6
NizuSAO <93949428+NizuSAO@users.noreply.github.com>
2022-10-31 19:25:41 -0300

    Update TestCalculadora.java

--- codigojava/calculadora/TestCalculadora.java
+++ codigojava/calculadora/TestCalculadora.java
@@ -20,16 +20,45 @@ public class TestCalculadora extends TestCase{
 		assertEquals(calculadora.sumar(2,2),(4));
 	}
 	
-	public void testeRestar () {
+	public void testeRestarEqual () {
 		escenario();
 		assertEquals(3, calculadora.resta(5, 2));;
 	}
+	public void testRestarFail () {
+		escenario();
+		assertNotEquals(calculadora.resta(3,2),3);
+	}
+	public void testRestar () {
+		escenario();
+		assertTrue(calculadora.resta(5,3),2);
+	
 	public void testMultiplicar (){
 		escenario();
 		assertTrue(calculadora.multiplicar(2,2)==(2*2));
 	}
+		
+	public void testMultiplicarFail (){
+		escenario();
+		assertNotEquals(calculadora.multiplicar(2,2),1);
+	}
+		
+	public void testMultiplicarEqual (){
+		escenario();
+		assertEquals(calculadora.multiplicar(2,2)4);
+	}
+		
 	public void testDividir () {
 		escenario();
 		assertTrue(calculadora.division(4, 2)==(4/2));
+	}
+		
+	public void testDividirFail (){
+		escenario();
+		assertNotEquals(calculadora.division(4,2),3);
+	}
+		
+	public void testDividirEquals (){
+		escenario();
+		assertEquals(calculadora.division(4,2)2);
 	}
 }
