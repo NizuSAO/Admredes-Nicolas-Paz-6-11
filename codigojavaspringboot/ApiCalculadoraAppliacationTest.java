832e518b854d7b2dc33956f41d9ed0852dd914f0
NizuSAO <93949428+NizuSAO@users.noreply.github.com>
2022-11-14 19:29:18 -0300

    Update ApiCalculadoraAppliacationTest.java

--- codigojavaspringboot/ApiCalculadoraAppliacationTest.java
+++ codigojavaspringboot/ApiCalculadoraAppliacationTest.java
@@ -19,54 +19,49 @@ class ApiCalculadoraApplicationTests {
    }
 
    @Test
-   void sumar() {
+	void sumar() {
 
-      assertEquals(calc.sumar(1,2),3);
-      assertNotEquals(calc.sumar(1,2),4);
-   }
-   
-    @Test
-   void resta() {
-      assertEquals(calc.resta(3,1),2);
-      assertNotEquals(calc.resta(3,2),2);
-   }
-   @Test
-   public void shouldThrowExpection() {
-      Assertions.assertThrows(ArithmeticException.class,()-->{
-            calc.resta(0,0);
-      });
+		assertEquals(calc.sumar(1, 2), 3);
+		assertNotEquals(calc.sumar(1, 2), 4);
+	}
 
-      
-      
-   @Test
-   void dividir() {
-      assertEquals(calc.dividir(4,2),2);
-      assertNotEquals(calc.dividir(3,3),3);
-   }
-      
-   @Test //(expected = ArithmeticException.class)
-   public void shouldThrowException() {
-      Assertions.assertThrows(ArithmeticException.class,()->{
-            calc.dividir(1,0);
-      });
-      
-   }
-   
-   @Test
-   void multiplicar() {
-      assertEquals(calc.multiplicar(2,2),4);
-      assertNotEquals(calc.multiplicar(2,3),5);
-   }
-      
-   @Test
-   void cuadratica() {
-      assertEquals(calc.cuadratica(1,2,1)==(-1));
-      assertNotEquals(calc.cuadratiuca(0,20,1),null);
-   }
-   @Test
-   public void shouldThrowException() {
-      Assertions.assertThrows(ArithmeticException.class,()->{
-            calc.cuadratica(5,1,3);
-      });
-      
-   }
+	@Test
+	void resta() {
+		assertEquals(calc.restar(3, 1), 2);
+		assertNotEquals(calc.restar(3, 2), 2);
+	}
+
+	@Test
+	void dividir() {
+		assertEquals(calc.dividir(4, 2), 2);
+		assertNotEquals(calc.dividir(3, 3), 3);
+	}
+
+	@Test //(expected = ArithmeticException.class)
+	public void expecionDividir() {
+		Assertions.assertThrows(ArithmeticException.class, ()-> {
+			calc.dividir(1, 0);
+		});
+
+	}
+
+	@Test
+	void multiplicar() {
+		assertEquals(calc.multiplicar(2, 2), 4);
+		assertNotEquals(calc.multiplicar(2, 3), 5);
+	}
+
+	@Test
+	void cuadratica() {
+		assertEquals(calc.cuadratica(1, 2, 1),(-1));
+		assertNotEquals(calc.cuadratica(0, 20, 1), null);
+	}
+
+	@Test
+	public void ExcepcionCuadratica() {
+		Assertions.assertThrows(ArithmeticException.class, () -> {
+			calc.cuadratica(5, 1, 3);
+		});
+
+	}
+}
