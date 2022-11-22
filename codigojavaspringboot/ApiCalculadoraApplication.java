46d70a4f4a3c1309935186a1f254c29483d25619
NizuSAO <93949428+NizuSAO@users.noreply.github.com>
2022-11-21 21:28:21 -0300

    Update ApiCalculadoraApplication.java

--- codigojavaspringboot/ApiCalculadoraApplication.java
+++ codigojavaspringboot/ApiCalculadoraApplication.java
@@ -2,15 +2,12 @@ package apiCalculadora.apiCalculadora;
 /*
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 public class ApiCalculadoraApplication {
-
-   public static void main(String[] args) {
-      SpringApplication.run(ApiCalculadoraApplication.class, args);
-      //System.out.println("Hola mundo");
-   }
-
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCalculadoraApplication.class, args);
+		//System.out.println("Hola mundo");
+	}
 }*/
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -22,36 +19,41 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ApiCalculadoraApplication {
 
-   public static void main(String[] args) {
-      SpringApplication.run(ApiCalculadoraApplication.class, args);
-   }
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCalculadoraApplication.class, args);
+	}
 
-   @GetMapping("/hola")
-   public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-      return String.format("Hello %s!", name);
-   }
+	@GetMapping("/sumar")
+	public int sumar(@RequestParam(value="a") int numero1,@RequestParam(value = "b")int numero2) {
+		return numero1 + numero2;
+	}
 
-   @GetMapping("/sumar")
-   public int sumar(@RequestParam(value="a") int numero1,@RequestParam(value = "b")int numero2) {
-      return numero1 + numero2;
-   }
+	@GetMapping("/restar")
+	public int restar(@RequestParam(value="a") int numero1,@RequestParam(value = "b")int numero2) {
+		return numero1 - numero2;
+	}
 
-   @GetMapping("/restar")
-   public int restar(@RequestParam(value="a") int numero1,@RequestParam(value = "b")int numero2) {
-      return numero1 - numero2;
-   }
+	@GetMapping("/multiplicar")
+	public int multiplicar(@RequestParam(value="a") int numero1,@RequestParam(value = "b")int numero2) {
+		return numero1 * numero2;
+	}
 
-   @GetMapping("/multiplicar")
-   public int multiplicar(@RequestParam(value="a") int numero1,@RequestParam(value = "b")int numero2) {
-      return numero1 * numero2;
-   }
+	@GetMapping("/dividir")
+	public int dividir(@RequestParam(value="a") int numero1,@RequestParam(value = "b")int numero2) {
+		return numero1 / numero2;
+	}
 
-   @GetMapping("/dividir")
-   public int dividir(@RequestParam(value="a") int numero1,@RequestParam(value = "b")int numero2) {
-      if (num2 == 0) {
-			throw new ArithmeticException("No se puede dividir por 0");
-		}
-		return num1 / num2;
-   }
+	@GetMapping("/cuadratica")
+	public double[] cuadratica(@RequestParam(value="a") int a,@RequestParam(value = "b")int b,@RequestParam(value = "c")int c) {
+		if(a ==0 || b == 0)  { throw new ArithmeticException(); }
+
+		double preMasMenos = Math.sqrt((b * b) - (4 * a * c));
+		double postMasMenosPositivo = (-b + preMasMenos)/2*a;
+		double postMasMenosNegativo = (-b - preMasMenos)/2*a;
+
+		double[] rta = {postMasMenosPositivo,postMasMenosNegativo};
+		return rta;
+
+	}
 
 }
